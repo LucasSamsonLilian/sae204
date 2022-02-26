d08e9124df4c69fdaff08291336ab30f2c522b8c
superlulu90 <lucas.ferry.ecole@gmail.com>
2022-02-26 11:41:38 +0100

    pour pythonanywhere

--- FlaskSAE204/connexion_db.py
+++ FlaskSAE204/connexion_db.py
@@ -1,17 +1,14 @@
 from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
-
 import pymysql.cursors
-
-
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = pymysql.connect(
-            host="localhost",
+            host="FerryDupuySchott.mysql.pythonanywhere-services.com",
             # host="serveurmysql",
-            user="lferry5",
-            password="1712",
-            database="BDD_sae204",
+            user="FerryDupuySchott",
+            password="Sae_204mgl",
+            database="FerryDupuySchott$BDD_sae204",
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
